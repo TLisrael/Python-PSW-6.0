04afa30bcd001fed00e76c6f78b861db65c5b563
Thainá Israel <thainaluizaisrael@gmail.com>
2023-04-10 16:39:02 -0300

    Aula 1 finalizada

---
+++ eventos/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Evento(models.Model):
+    criador = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    nome = models.CharField(max_length=256)
+    descricao = models.TextField()
+    data_inicio = models.DateField()
+    data_termino = models.DateField()
+    carga_horaria = models.IntegerField()
+    logo = models.ImageField(upload_to="logos")
+    
+    # paleta de cores 
+    cor_principal = models.CharField(max_length=7)
+    cor_secundaria = models.CharField(max_length=7)
+    cor_fundo = models.CharField(max_length=7)
+    
+    def __str__(self) -> str:
+        return self.nome
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('cadastro/', views.cadastro, name="cadastro"), 
     path('login/', views.login, name='login'),
+    
 ]
