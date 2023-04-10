c2fd3952657cf2f0287534d0a55c5e182200c008
Thainá Israel <thainaluizaisrael@gmail.com>
2023-04-10 13:04:34 -0300

    Iniciando o html

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
 # Create your views here.
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
