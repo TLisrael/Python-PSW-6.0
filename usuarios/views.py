6c1770894e16e6e5d4a8123d30c1d36be70f47a5
Thainá Israel <thainaluizaisrael@gmail.com>
2023-04-10 15:25:39 -0300

    Finalizando parte de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, request
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.urls import reverse
 
 # Create your views here.
 def cadastro(request):
@@ -12,5 +16,16 @@ def cadastro(request):
         confirm_senha = request.POST.get('confirmar_senha')
         
         if not senha == confirm_senha:
-            return redirect('/usuarios/cadastro')
-    return HttpResponse('Teste')
+            messages.add_message(request, constants.ERROR, 'As senhas não são iguais')
+            return redirect(reverse(cadastro))
+            
+        
+        
+        user = User.objects.filter(username=username)
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Nome de usuário já cadastrado')
+            return redirect(reverse(cadastro))
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+
+        return redirect(reverse(login))
